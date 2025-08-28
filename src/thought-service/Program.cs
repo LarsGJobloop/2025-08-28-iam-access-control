dd53354d7ab4807624efbb06f2a4f95bad47eb2d
zabronax <zabronax@gmail.com>
2025-08-28 11:04:53 +0200

    setup jwt authentication

--- src/thought-service/Program.cs
+++ src/thought-service/Program.cs
@@ -1,10 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtOptions =>
+    {
+        // Who have issued the token (your OIDC provider)
+        jwtOptions.Authority = "oidc.jobloop.com";
+        // Your application usually (the site the app is hosted under)
+        jwtOptions.Audience = "localhost";
+    });
 
 var app = builder.Build();
 
